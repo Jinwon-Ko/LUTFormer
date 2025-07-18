30b0e650ba9f3521aecc6c8004297e3883c8b97a
JinwonKo <115671998+Jinwon-Ko@users.noreply.github.com>
2025-07-18 23:48:32 +0900

    Update evaluate_PPR10K.sh

--- code/evaluate_PPR10K.sh
+++ code/evaluate_PPR10K.sh
@@ -1,4 +1,4 @@
 python main.py
 
 # Check your paths with expert ['A', 'B', 'C']
-matlab -nodisplay -nosplash -nodesktop -r "ppr10k_calculate_metrics('../v00_output/display/Retouching_PPR10K_expertA/analysis/pred', '../v00_output/display/Retouching_PPR10K_expertA/analysis/gt', 'PATH_FOR_MASK'); exit;"
+matlab -nodisplay -nosplash -nodesktop -r "ppr10k_calculate_metrics('../output/display/Retouching_PPR10K_expertA/analysis/pred', '../output/display/Retouching_PPR10K_expertA/analysis/gt', 'PATH_FOR_MASK'); exit;"
