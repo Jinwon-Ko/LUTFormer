e59160fad3fd6961af991c85a10fbe4848683f23
JinwonKo <115671998+Jinwon-Ko@users.noreply.github.com>
2025-07-19 03:05:13 +0900

    Delete code directory

--- code/evaluate_FiveK.sh
+++
@@ -1,3 +0,0 @@
-python main.py
-
-matlab -nodisplay -nosplash -nodesktop -r "fivek_calculate_metrics('../output/display/Retouching_FiveK_expertC/analysis/pred', '../output/display/Retouching_FiveK_expertC/analysis/gt'); exit;"
