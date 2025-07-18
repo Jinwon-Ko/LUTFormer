bc769c554db4c2070f9328635fa7ba0e76e0f9e1
JinwonKo <115671998+Jinwon-Ko@users.noreply.github.com>
2025-07-18 23:48:43 +0900

    Update evaluate_FiveK.sh

--- code/evaluate_FiveK.sh
+++ code/evaluate_FiveK.sh
@@ -1,3 +1,3 @@
 python main.py
 
-matlab -nodisplay -nosplash -nodesktop -r "fivek_calculate_metrics('../v00_output/display/Retouching_FiveK_expertC/analysis/pred', '../v00_output/display/Retouching_FiveK_expertC/analysis/gt'); exit;"
+matlab -nodisplay -nosplash -nodesktop -r "fivek_calculate_metrics('../output/display/Retouching_FiveK_expertC/analysis/pred', '../output/display/Retouching_FiveK_expertC/analysis/gt'); exit;"
